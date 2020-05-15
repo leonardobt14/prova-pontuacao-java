9555221e7e1a2325ffbc14834708abceff1bce71
leonardobt14 <leonardo3ktt@gmail.com>
2020-05-15 19:23:40 -0300

    Leitura do volume

--- src/Projeto.java
+++ src/Projeto.java
@@ -7,10 +7,20 @@ public class Projeto {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		int compras;
+		double ticketmedio;
 
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		compras = sc.nextInt();
+		System.out.print("Qual o ticket médio? ");
+		ticketmedio = sc.nextDouble();
+		
+		
 		
 		sc.close();
 	}
