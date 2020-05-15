a8624ff74c95c835f0185f0555716f3741473e86
leonardobt14 <leonardo3ktt@gmail.com>
2020-05-15 19:18:32 -0300

    Cabecalho

---
+++ src/Projeto.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Projeto {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		sc.close();
+	}
+
+}
+
+	
